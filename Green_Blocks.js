ea1c66ca846f0d3468a1b67ecde31c4e4de47fce
aanvi20 <72300637+aanvi20@users.noreply.github.com>
2020-11-26 15:12:44 +0530

    Add files via upload

---
+++ Green_Blocks.js
@@ -0,0 +1,28 @@
+class Green_Block{
+    constructor(x, y, width, height){
+        var options={
+           'restitution': 0.8,
+           'friction': 1,
+           'density': 1.2,
+           'isStatic': false
+        }
+
+        this.body=Bodies.rectangle(x, y, width, height, options);
+        this.image = loadImage("green_circle.png")
+        this.width=width
+        this.height=height
+        World.add(world, this.body)
+ 
+       
+       
+    }
+
+    display(){
+        var pos=this.body.position
+        imageMode(CENTER)
+        stroke ("green");
+        fill ("yellow");
+        image (this.image, pos.x, pos.y, this.width, this.height)
+       // rect(this.body.position.x, this.body.position.y, this.width, this.height)
+    }
+}
